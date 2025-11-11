8d4c82a673462fbbd7497fd55119073cf8d888b4
Sunil Yadav <sunilyadav95328@gmail.com>
2025-11-11 19:58:54 +0530

    Add backend configuration for Azure RM provider in Terraform

--- Environment/Dev/provider.tf
+++ Environment/Dev/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.52.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name   = "rg-tfstate"
+    storage_account_name  = "tfstateinfra123"
+    container_name        = "tfstate"
+    key                   = "todoapp-dev.tfstate"
+  }
 }
 
 provider "azurerm" {
